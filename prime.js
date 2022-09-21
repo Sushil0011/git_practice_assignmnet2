cfd6d50e34f3a3ed1f441aa7e26d2257ab7cf75d
SUshil Tripathi <yashtripathi861@gmail.com>
2022-09-21 16:27:51 +0530

    fixed it

--- prime.js
+++ prime.js
@@ -1,7 +1,10 @@
 function isprime(num){
+    if(num<=0||num==1){
+        return "Not a prime"
+    }
     for(let i=2;i<=num**0.5;i++){
         if(num%i==0){
-            return "NO";
+            return "Not a Prime";
 
         }
     }
